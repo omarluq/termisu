3379f901bb1be7407a607814b674d8a1d3261e7b
omarluq <omar.luqman@hey.com>
2025-11-14 23:22:12 -0600

    implement flush, write and outfd/infd in TTY

--- src/termisu/tty.cr
+++ src/termisu/tty.cr
@@ -1,9 +1,5 @@
 # Low-level TTY (terminal) interface for reading from and writing to `/dev/tty`.
 #
-# This class handles platform-specific differences in TTY access:
-# - On OpenBSD/FreeBSD: uses a single read/write file descriptor
-# - On Linux/others: uses separate read and write file descriptors
-#
 # Example:
 # ```
 # tty = Termisu::TTY.new
@@ -15,6 +11,8 @@ class Termisu::TTY
 
   @out : File
   @in : File | Int32
+  @outfd : Int32
+  @infd : Int32
 
   {% begin %}
     {% bsd = flag?(:openbsd) || flag?(:freebsd) %}
@@ -22,18 +20,30 @@ class Termisu::TTY
     private FILE_MODE = {{ bsd ? "r+" : "w" }}
   {% end %}
 
+  getter outfd, infd
+
   # Opens `/dev/tty` for terminal access.
   #
   # Raises `IO::Error` if the TTY cannot be opened.
   def initialize
     @out = File.open(PATH, FILE_MODE)
     @in = USE_RDWR ? @out : open_readonly_fd
+    @outfd = @out.fd
+    @infd = USE_RDWR ? @outfd : @in.as(Int32)
   end
 
   # Closes the TTY file descriptors.
   def close
     close_output_fd
     close_input_fd unless USE_RDWR
+  end
+
+  def write(data : String)
+    @out.print(data)
+  end
+
+  def flush
+    @out.flush
   end
 
   private def open_readonly_fd : Int32
