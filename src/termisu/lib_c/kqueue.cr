2dac4fd9e5a5cb3042b69b926a295193271789f7
omarluq <omar.luqman@hey.com>
2025-12-16 21:29:30 -0600

    Remove duplicate Kevent struct definition

--- src/termisu/lib_c/kqueue.cr
+++ src/termisu/lib_c/kqueue.cr
@@ -1,28 +1,10 @@
 # LibC bindings for kqueue on BSD systems (FreeBSD, OpenBSD).
 #
-# Crystal's stdlib provides kqueue constants (EVFILT_*, EV_*) but not
-# the kqueue/kevent functions or Kevent struct for FreeBSD/OpenBSD.
-#
-# FreeBSD kevent struct (from sys/event.h):
-#   uintptr_t ident     - identifier for this event
-#   short     filter    - filter for event
-#   u_short   flags     - action flags for kqueue
-#   u_int     fflags    - filter flag value
-#   intptr_t  data      - filter data value
-#   void*     udata     - opaque user data identifier
+# Crystal's stdlib provides kqueue constants (EVFILT_*, EV_*) and the
+# Kevent struct, but not the kqueue/kevent functions for FreeBSD/OpenBSD.
 
 {% if flag?(:freebsd) || flag?(:openbsd) %}
   lib LibC
-    # kevent structure - matches FreeBSD sys/event.h
-    struct Kevent
-      ident : SizeT   # uintptr_t - identifier for this event
-      filter : Int16  # short - filter for event
-      flags : UInt16  # u_short - action flags for kqueue
-      fflags : UInt32 # u_int - filter flag value
-      data : SSizeT   # intptr_t - filter data value
-      udata : Void*   # opaque user data identifier
-    end
-
     # Create a new kernel event queue
     fun kqueue : Int32
 
