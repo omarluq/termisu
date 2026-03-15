e5cb7b683d1a99cc81e30c759f30e30a3cad51e2
Omar Alani <omar.luqman@hey.com>
2026-03-14 21:44:00 -0500

    refactor: consolidate FFI guard fallback handling

--- src/termisu/ffi/guards.cr
+++ src/termisu/ffi/guards.cr
@@ -1,23 +1,17 @@
 module Termisu::FFI::Guards
   def self.safe_handle(& : -> UInt64) : UInt64
-    Termisu::FFI::Runtime.ensure_initialized
-    Termisu::FFI::ErrorState.clear
-    yield
-  rescue ex
-    Termisu::FFI::ErrorState.set(Termisu::FFI::ErrorState.format(ex))
-    0_u64
+    with_error_fallback(0_u64) { yield }
   end
 
   def self.safe_status(& : -> Termisu::FFI::Status) : Int32
-    Termisu::FFI::Runtime.ensure_initialized
-    Termisu::FFI::ErrorState.clear
-    yield.value
-  rescue ex
-    Termisu::FFI::ErrorState.set(Termisu::FFI::ErrorState.format(ex))
-    Termisu::FFI::Status::Error.value
+    with_error_fallback(Termisu::FFI::Status::Error) { yield }.value
   end
 
   def self.safe_u8(default : UInt8 = 0_u8, & : -> UInt8) : UInt8
+    with_error_fallback(default) { yield }
+  end
+
+  private def self.with_error_fallback(default : T, & : -> T) : T forall T
     Termisu::FFI::Runtime.ensure_initialized
     Termisu::FFI::ErrorState.clear
     yield
